81fcbab9d0a141d4c306b4194360d1af00c9dbc3
Kevin Williams <KingArthur5o3x@gmx.com>
2024-01-14 23:14:44 -0600

    solved problem 9

---
+++ basic_exercises/problem9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    printf("Input the first integer: ");
+    scanf("%d", &a);
+    printf("\nInput the second integer: ");
+    scanf("%d", &b);
+
+    printf("\nSum of the above two integers = %d\n", a + b);
+
+    return 0;
+}
