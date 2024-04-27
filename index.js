fe5ac0dfab332b5aa479d71dc253f554550f99ee
Kevin Williams <KingArthur5o3x@gmx.com>
2024-04-27 00:17:45 -0500

    update webpage

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ function updateProgressBar(folderName, totalExercises) {
     const fillElement = document.getElementById(`${folderName}-fill`);
     const labelElement = document.getElementById(`${folderName}-label`);
 
-    // Simulate the number of exercises completed (you can replace this with actual logic)
     const completedExercises = Math.floor(Math.random() * totalExercises) + 1;
 
-    // Calculate percentage
     const percentage = (completedExercises / totalExercises) * 100;
 
-    // Update progress bar
     fillElement.style.width = `${percentage}%`;
     labelElement.innerText = `${completedExercises}/${totalExercises} (${percentage.toFixed(2)}%)`;
 }
 
-// Update progress bars for each topic
-updateProgressBar('basic-exercises', 157);
+updateProgressBar('basic_exercises', 157);
+updateProgressBar('basic_algorithm', 75);
+updateProgressBar('tpyes', 18);
+updateProgressBar('input_output', 10);
+updateProgressBar('for_loop', 61);
